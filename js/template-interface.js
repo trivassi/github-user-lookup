3e59d570274071a1081c63cda4fd8c77d94c608b
Thomas Rivas <rivassi.thomas@gmail.com>
2016-10-14 11:29:51 -0700

    create form and jquery for search

--- js/template-interface.js
+++ js/template-interface.js
@@ -2,5 +2,8 @@ var ObjectName = require('./../js/template.js').objectModule;
 //var namedoesntmatter = require(path).namehastomatchbackendfile
 
 $(document).ready(function() {
+  $('search').submit(event(){
+    
+  });
   event.preventDefault();
 });
