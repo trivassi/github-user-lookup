46295538997947b246eb2d22729961e7a2842035
Thomas Rivas <rivassi.thomas@gmail.com>
2016-10-17 08:21:55 -0700

    add frontend funcitonaity

--- js/user-interface.js
+++ js/user-interface.js
@@ -4,14 +4,18 @@ var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
   $("#search" ).submit(function(event) {
-        // $("#form-login").submit(function (event) {
     event.preventDefault();
 
-        var username = $('#username').val(), user = new userModule(username, userModule);
+        var username = $('#username').val(), user = new userModule(username);
         $('#username').val("");
 
+        user.getRepos().then(function (response) {
+           $("#userreturn").append('<h2>Total Repositories :" >'+response.length+'</h2>');
 
-
+           response.forEach(function (objRepo) {
+                   $("#userreturn").append("h4>" +  objRepo.html_url+ "</h4>" + "<ul>" + "<li>" + objRepo.name + objRepo.description + "</li> </ul>" );
+               });
+             });
 // https://api.github.com
 
   });
