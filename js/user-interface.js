60865d2ee29857ba0cf387e353498de659bef79c
Thomas Rivas <rivassi.thomas@gmail.com>
2016-10-16 23:32:15 -0700

    add beginning animation

--- js/user-interface.js
+++ js/user-interface.js
@@ -8,8 +8,20 @@ $(document).ready(function() {
     event.preventDefault();
 
         var username = $('#username').val(), user = new userModule(username, userModule);
+        $('#username').val("");
 
 
+        api.getRepositories().then(function (response) {
+            $("#total-repos").append('<h3>Total Repositories : <span class="label label-danger" >'+response.length+'</span></h3>');
+
+            response.forEach(function (objRepo) {
+                $("#response-github").append("<div class='row repository'><div class='col-md-12'><h3 class='form-section'><a href='"+objRepo.html_url+"' target='_blank'>"+objRepo.name+" <b>[ "+objRepo.id+" ]</b></a></h3><p>"+objRepo.description+"</p></div></div>");
+            });
+        }).catch(function (reason) {
+            console.log(reason);
+        });
+    });
+})();
 
 // https://api.github.com
 
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,15 +1,30 @@
-$(document).ready(function(){
-  $('#signup').submit(function(event){
-    event.preventDefault();
-    var email = $('#email').val();
-    $('#signup').hide();
-    $('#solution').prepend('<p>Thank you, ' + email + ' has been added to our list!</p>');
-  });
-});
-
-var Object = require('./../js/template.js').objectModule;
+var User = require('./../js/user.js').userModule;
+var apiKey = require('./../.env').apiKey;
 //var namedoesntmatter = require(path).namehastomatchbackendfile
 
 $(document).ready(function() {
+  $("#search" ).submit(function(event) {
+        // $("#form-login").submit(function (event) {
+    event.preventDefault();
+
+        var username = $('#username').val(), user = new userModule(username, userModule);
+        $('#username').val("");
+
+
+        api.getRepositories().then(function (response) {
+            $("#total-repos").append('<h3>Total Repositories : <span class="label label-danger" >'+response.length+'</span></h3>');
+
+            response.forEach(function (objRepo) {
+                $("#response-github").append("<div class='row repository'><div class='col-md-12'><h3 class='form-section'><a href='"+objRepo.html_url+"' target='_blank'>"+objRepo.name+" <b>[ "+objRepo.id+" ]</b></a></h3><p>"+objRepo.description+"</p></div></div>");
+            });
+        }).catch(function (reason) {
+            console.log(reason);
+        });
+    });
+})();
+
+// https://api.github.com
+
+  });
   event.preventDefault();
 });
