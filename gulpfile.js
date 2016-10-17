e9bf35fab7d05c92628d6862857a58ef6026448e
Thomas Rivas <rivassi.thomas@gmail.com>
2016-10-17 08:09:55 -0700

    fix gulp build issue

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var utilities = require('gulp-util');
 var buildProduction = utilities.env.production;
 var jshint = require('gulp-jshint');
 var del = require('del');
+
 var lib = require('bower-files')({
   "overrides":{
     "bootstrap" : {
--- js/user-interface.js
+++ js/user-interface.js
@@ -11,17 +11,6 @@ $(document).ready(function() {
         $('#username').val("");
 
 
-        api.getRepositories().then(function (response) {
-            $("#total-repos").append('<h3>Total Repositories : <span class="label label-danger" >'+response.length+'</span></h3>');
-
-            response.forEach(function (objRepo) {
-                $("#response-github").append("<div class='row repository'><div class='col-md-12'><h3 class='form-section'><a href='"+objRepo.html_url+"' target='_blank'>"+objRepo.name+" <b>[ "+objRepo.id+" ]</b></a></h3><p>"+objRepo.description+"</p></div></div>");
-            });
-        }).catch(function (reason) {
-            console.log(reason);
-        });
-    });
-})();
 
 // https://api.github.com
 
